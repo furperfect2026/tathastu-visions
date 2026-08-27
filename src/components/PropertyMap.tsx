0082dafbbf7b5faeaf42eae414e584dfe53f3194
furperfect2026 <furperfect2024@gmail.com>
2026-08-27 12:23:01 +0530

    Fix: Remove CARTO map watermark by switching to Esri Light Gray Canvas tiles

--- src/components/PropertyMap.tsx
+++ src/components/PropertyMap.tsx
@@ -87,8 +87,8 @@ export default function PropertyMap({
         scrollWheelZoom={false}
       >
         <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-          url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+          attribution='Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ'
+          url="https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}"
         />
 
         {properties.map((p) => {
